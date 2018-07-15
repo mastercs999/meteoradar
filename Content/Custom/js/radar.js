94732c2fddb6f89857c8105333d0378504480873
Unknown <mastercs999@seznam.cz>
2018-07-15 20:34:58 +0200

    Styling, detail day/time selection

--- Content/Custom/js/radar.js
+++ Content/Custom/js/radar.js
@@ -1,5 +1,8 @@
 function Radar(wrapper, initDateTime, getUrlFunc) {
 
+    // TODO
+    // Style
+
     // Constants
     var FIRST_SNAPSHOTS = 6;
     var ANIMATION_DELAY = 500;
@@ -8,6 +11,7 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
     var animationHandler = null;
     var imageIds = [];
     var animatedIndex = 0;
+    var imgCache = {};
 
     // Current snapshots
     var snapshots = {};
@@ -31,11 +35,25 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
              pause();
     });
     wrapper.find(".stop").click(stop);
+
+    // Set callback on date and time change
+    wrapper.find(".side input").add(wrapper.find(".side .hours")).change(function() {
+        var date = getSelectedDateTime();
+
+        // Download snapshots and play
+        downloadSnapshots(date, function() {
+            prepareControls();
+            play();
+        });
+    })
     
     // Show map
     var imageLayer = initMap(); 
 
-    // First download possible snapshots for last 2 days
+    // Init controls
+    initControls(initDateTime);
+
+    // First download snapshot and play the animation
     downloadSnapshots(initDateTime, function() {
         prepareControls();
         play();
@@ -108,20 +126,34 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
     // Set current image on the map and syncs time holder
     function setSnapshot(index) {
         
-        // Set image
-        var id = imageLayer.addImage(snapshots["images"][index]["url"], SMap.Coords.fromWGS84(snapshots["start-lon"], snapshots["start-lat"]), SMap.Coords.fromWGS84(snapshots["end-lon"], snapshots["end-lat"]));
-        
-        // Remove previous images
-        setTimeout(function() {
-            for (var i in imageIds)
-                imageLayer.removeImage(imageIds[i]);
-            imageIds.length = 0
+        function _setSnapshot(url) {
+            var id = imageLayer.addImage(url, SMap.Coords.fromWGS84(snapshots["start-lon"], snapshots["start-lat"]), SMap.Coords.fromWGS84(snapshots["end-lon"], snapshots["end-lat"]));
+            
+            // Remove previous images
+            setTimeout(function() {
+                for (var i in imageIds)
+                    imageLayer.removeImage(imageIds[i]);
+                imageIds.length = 0
 
-            imageIds.push(id);
-        }, 1);
+                imageIds.push(id);
+            }, 1);
 
-        // Sync time holder
-        wrapper.find(".time-holder").text(formatDateTime(snapshots["images"][index]["datetime"]));
+            // Sync time holder
+            wrapper.find(".time-holder").text(formatDateTime(snapshots["images"][index]["datetime"]));
+        }
+
+        // Either get image from cache or convert image to base64
+        var url = snapshots["images"][index]["url"];
+        if (imgCache[url])
+            _setSnapshot(imgCache[url]);
+        else {
+            _setSnapshot(url);
+
+            // Convert and save to cache
+            imgToDataUrl(url, function(img) {
+                imgCache[url] = img;
+            })
+        }
     }
 
     // Prepares map and controls by snapshots
@@ -143,6 +175,9 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
             select.prepend(opt);
         }
 
+        // Select proper hour
+        wrapper.find(".side .hours").val(snapshots["images"][0]["datetime"].getHours());
+
         // Select first 6
         select.find("option").slice(0, FIRST_SNAPSHOTS).attr('selected', true);
         animatedIndex = 0;
@@ -161,6 +196,14 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
     // Gets index of selected snapshots in slider
     function getSliderIndex() {
         return parseInt(wrapper.find(".slider-selection").val());
+    }
+
+    // Gets date and time which were selected
+    function getSelectedDateTime() {
+        var date = wrapper.find(".side input")[0].valueAsDate;
+        date.setHours(wrapper.find(".side .hours").val())
+        
+        return date;
     }
 
     // Download possible snapshots for given date
@@ -185,7 +228,7 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
                     "datetime": datetime,
                     "url" : url
                 };
-
+                
                 snapshots["images"].push(snapshot);
             }
 
@@ -199,11 +242,12 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
         // Set map width
         var mapDiv = wrapper.find(".czMap");
         mapDiv.width(mapDiv.parent().width() - mapDiv.next().width())
+        mapDiv.height(window.innerHeight - wrapper.find(".under").outerHeight())
 
         // Init and center
         var center = SMap.Coords.fromWGS84(15.472962, 49.817492);
 
-        var map = new SMap(JAK.gel(mapDiv[0]), center, 7);
+        var map = new SMap(JAK.gel(mapDiv[0]), center, 8);
         map.addDefaultLayer(SMap.DEF_BASE).enable();
         map.addDefaultControls();
 
@@ -212,13 +256,42 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
         map.addLayer(imageLayer, false);                     
         imageLayer.enable();
         $(imageLayer.getContainer()[0].parentElement).addClass("mapy-layers");
-        
+
+        // Add HTML layer
+        var htmlLayer = new SMap.Layer.HUD();
+        map.addLayer(htmlLayer, false);
+        htmlLayer.enable();
+        htmlLayer.addItem($("<h2 class=\"time-holder\"></h>")[0], { top: "-2px", right: "100px"}, true);
+
         return imageLayer;
+    }
+
+    // Initializes controls
+    function initControls(date) {
+
+         // Set date to today
+         var datePicker = wrapper.find(".side input");
+         var dateValue = datePicker.val();
+         if (datePicker.length > 0 && (!dateValue || dateValue.length == 0))
+             datePicker.val(formatInputDate(date));
+
+        // Set options to hours select
+        var hourPicker = wrapper.find(".side .hours");
+        for (i = 0; i < 24; ++i)
+            hourPicker.append($("<option>").val(i).text(i.toString().padStart(2, "0") + ":00"))
+
+        // Set times height
+        wrapper.find(".times").height(window.innerHeight - wrapper.find(".under").outerHeight() - (datePicker.length > 0 ? datePicker.outerHeight() : 0) - (hourPicker.length > 0 ? hourPicker.outerHeight() : 0));
     }
 
     // Returns string representation of datetime
     function formatDateTime(datetime) {
         return datetime.getDate().toString().padStart(2, "0") + "." + (datetime.getMonth() + 1).toString().padStart(2, "0") + ". " + datetime.getHours().toString().padStart(2, "0") + ":" + datetime.getMinutes().toString().padStart(2, "0");
+    }
+
+    // Formats date to format suitable for input of type date
+    function formatInputDate(date) {
+        return date.getFullYear().toString().padStart(2, "0") + "-" + (date.getMonth() + 1).toString().padStart(2, "0") + "-" + date.getDate().toString().padStart(2, "0")
     }
 
     // Downloads file from given url and then calls callback with the result
@@ -232,7 +305,30 @@ function Radar(wrapper, initDateTime, getUrlFunc) {
         xmlHttp.open("GET", url, true);
         xmlHttp.send(null);
     }
+
 }
+
+// Converts image to its data url
+function imgToDataUrl(src, callback) {
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function() {
+        var canvas = document.createElement('CANVAS');
+        var ctx = canvas.getContext('2d');
+        canvas.height = this.naturalHeight;
+        canvas.width = this.naturalWidth;
+        ctx.drawImage(this, 0, 0);
+        var dataURL = canvas.toDataURL();
+        callback(dataURL);
+    };
+    img.src = src;
+    
+    if (img.complete || img.complete === undefined) {
+        img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        img.src = src;
+    }
+}
+
 
 // Converts unix time stamp to JS date
 function fromUnixTime(timestamp) {
